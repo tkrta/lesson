02aeeee58df381e5f7e348980f6bb035369af2f1
tkrta <tkrt1129@gmail.com>
2022-12-24 13:42:33 +0900

    lesson8_2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\PostController;
-
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;      //追加
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,5 +12,4 @@ use App\Http\Controllers\PostController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/posts', [PostController::class, 'index']);
+Route::get('/', [PostController::class, 'index']);
