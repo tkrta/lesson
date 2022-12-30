277f8d250191eec498979c1fcdf32e5859deac27
tkrta <tkrt1129@gmail.com>
2022-12-30 22:04:54 +0900

    Lesson9_2

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,8 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Blog</title>
+<title>Blog</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
@@ -37,5 +33,8 @@
                 }
             }
         </script>
+        <div class="footer">
+            <a href="/">戻る</a>
+        </div>
     </body>
 </html>
